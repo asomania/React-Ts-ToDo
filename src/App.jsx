696230dae75ee57b2416d2cfa84dbdc1e319db0c
Eren Küçük <erenkucuk2002@gmail.com>
2022-12-02 14:42:26 +0300

    toDO

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./Tailwind.css";
+import React from "react";
+import Todo from "./toDo";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex m-2 justify-center">
+        <input
+          type="text"
+          id="textBox"
+          className="border-2 border-indigo-600"
+        />
+        <button
+          className="border-2 border-indigo-600 w-12 ml-2"
+          onClick={() => setName(document.getElementById("textBox").value)}
+        >
+          Ekle
+        </button>
+        <br />
+      </div>
+      <div className=" flex-col justify-center">
+        <Todo name={name}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/navBar.js
+++
@@ -1,7 +0,0 @@
-function NavBar(){
-    return(
-        <h1>My Personal Basic Portfolio</h1>
-    );
-}
-
-export default NavBar;
