c3f308a2544ffff77410d4e88d097adf946dafc1
Eren Küçük <erenkucuk2002@gmail.com>
2023-03-30 02:00:47 +0300

    taskContent

--- src/context/taskContent.tsx
+++ src/context/taskContent.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 
-const taskContent = () => {
+type TaskContent = {
+  task: string;
+  setTask: (task: string) => void;
+  tasks: string[];
+  setTasks: (tasks: string[]) => void;
+  addTask: () => void;
+};
+type Props = {
+  children: React.ReactNode;
+};
+const taskContent = React.createContext<TaskContent>({
+  task: "",
+  setTask: (task: string) => {},
+  tasks: [],
+  setTasks: (tasks: string[]) => {},
+  addTask: () => {},
+});
+
+export const TaskProvider: React.FC<Props> = ({ children }) => {
+  const [task, setTask] = React.useState("");
+  const [tasks, setTasks] = React.useState<string[]>([]);
+  const addTask = () => {
+    setTasks([...tasks, task]);
+  };
   return (
-    <div>
-      <h1>Task Content</h1>
-    </div>
+    <taskContent.Provider value={{ task, setTask, tasks, setTasks, addTask }}>
+      {children}
+    </taskContent.Provider>
   );
 };
 
-export default taskContent;
+export const TaskContent = useContext(taskContent);
