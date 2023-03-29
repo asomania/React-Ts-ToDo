75a579e3f74aa8b43d71fddc5b90f2bb09986788
Eren Küçük <erenkucuk2002@gmail.com>
2023-03-29 20:25:21 +0300

    featuresContext edited

--- src/context/taskContent.tsx
+++ src/context/taskContent.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const taskContent = () => {
-  return <div>taskContent</div>;
+  return (
+    <div>
+      <h1>Task Content</h1>
+    </div>
+  );
 };
 
 export default taskContent;
